3b6e96941bb810943d1a88ad1633851e6269aaee
devnaine-code <dev.nine0@gmail.com>
2024-10-09 17:50:20 +0900

    Re build2

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3d93ee61f6ec62ff0adf2c58a32fd9cc.js"
+  "/portfolio/precache-manifest.32c7301ec77e0ed377a5f5ee20cc33b8.js"
 );
 
 self.addEventListener('message', (event) => {
