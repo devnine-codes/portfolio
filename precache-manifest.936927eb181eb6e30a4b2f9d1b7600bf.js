25ee0fcd6dce4ebf687019da05c8cfc651112292
devnine-codes <devnine-codes@users.noreply.github.com>
2024-10-10 06:59:46 +0000

    deploy: 0c42f8971308a5343953e8469dabc509e63f17e0

---
+++ precache-manifest.936927eb181eb6e30a4b2f9d1b7600bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52922cd844f45f91ff067f4a5459bf52",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be93f7e4f7d24d351bcd",
+    "url": "/portfolio/static/css/2.28f95363.chunk.css"
+  },
+  {
+    "revision": "b337983dd5c878611b91",
+    "url": "/portfolio/static/css/main.655ca30f.chunk.css"
+  },
+  {
+    "revision": "be93f7e4f7d24d351bcd",
+    "url": "/portfolio/static/js/2.5a4033e3.chunk.js"
+  },
+  {
+    "revision": "b654d2d8dc25a6836c51",
+    "url": "/portfolio/static/js/3.686d18a0.chunk.js"
+  },
+  {
+    "revision": "b337983dd5c878611b91",
+    "url": "/portfolio/static/js/main.e43900c0.chunk.js"
+  },
+  {
+    "revision": "4f6c9845a5186fc9a035",
+    "url": "/portfolio/static/js/runtime~main.e5c1708d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
